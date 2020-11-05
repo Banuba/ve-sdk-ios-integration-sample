bf7ae473e4a20405eceab989b34baea4acf567e7
ve-sdk-ios <sdk-ve-ios@banuba.com>
2020-11-05 16:31:58 +0300

    setup default configuration

--- Example/Example/ViewController.swift
+++ Example/Example/ViewController.swift
@@ -1,8 +1,40 @@
 import UIKit
+import BanubaVideoEditorSDK
 
 class ViewController: UIViewController {
+
+  private var videoEditorSDK: BanubaVideoEditorSDK?
   
   override func viewDidLoad() {
     super.viewDidLoad()
+    
+    initVideoEditor()
+  }
+  
+  private func initVideoEditor() {
+    // use default configuration
+    let config = VideoEditorConfig()
+    videoEditorSDK = BanubaVideoEditorSDK(
+      token: "place client token here",
+      effectsToken: "place effects token here",
+      configuration: config,
+      analytics: nil,
+      externalViewControllerFactory: nil
+    )
+    videoEditorSDK?.delegate = self
+  }
+  
+  @IBAction func openVideoEditorAction(_ sender: Any) {
+    videoEditorSDK?.presentVideoEditor(from: self, animated: true, completion: nil)
+  }
+}
+
+extension ViewController: BanubaVideoEditorSDKDelegate {
+  func videoEditorDidCancel(_ videoEditor: BanubaVideoEditorSDK) {
+    videoEditor.dismissVideoEditor(animated: true, completion: nil)
+  }
+  
+  func videoEditorDone(_ videoEditor: BanubaVideoEditorSDK) {
+    videoEditor.dismissVideoEditor(animated: true, completion: nil)
   }
 }
