ebcac670775848b1e4c258fbef61af813504455f
ve-sdk-ios <sdk-ve-ios@banuba.com>
2020-11-08 18:25:33 +0300

    video cover screen customization

--- Example/Example/ViewController.swift
+++ Example/Example/ViewController.swift
@@ -28,9 +28,41 @@ class ViewController: UIViewController {
   }
   
   func createVideoEditorConfiguration() -> VideoEditorConfig {
-    let config = VideoEditorConfig()
+    var config = VideoEditorConfig()
+    
+    config.videoCoverSelectionConfiguration = updateVideCoverSelectionConfiguration(config.videoCoverSelectionConfiguration)
     
     return config
+  }
+  
+  private func updateVideCoverSelectionConfiguration(_ configuration: VideoCoverSelectionConfiguration) -> VideoCoverSelectionConfiguration {
+    var configuration = configuration
+    
+    configuration.cancelButton = TextButtonConfiguration(
+      style: TextConfiguration(
+        font: UIFont.boldSystemFont(ofSize: 18.0),
+        color: UIColor(red: 6, green: 188, blue: 193)
+      ),
+      text: "Cancel"
+    )
+    configuration.doneButton = RoundedButtonConfiguration(
+      textConfiguration: TextConfiguration(
+        font: UIFont.boldSystemFont(ofSize: 18.0),
+        color: UIColor(red: 6, green: 188, blue: 193)
+      ),
+      cornerRadius: 0.0,
+      backgroundColor: .clear
+    )
+    configuration.sliderColor = UIColor(red: 6, green: 188, blue: 193)
+    configuration.sliderMinTrackTintColor = UIColor(red: 6, green: 188, blue: 193)
+    configuration.toolTipLabel = TextConfiguration(
+      kern: 0.0,
+      font: UIFont.systemFont(ofSize: 16.0),
+      color: .white,
+      alignment: .left
+    )
+    
+    return configuration
   }
 }
 
