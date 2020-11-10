1f436547f1d633ce0ef32c2e3c84f3358d7b4361
ve-sdk-ios <sdk-ve-ios@banuba.com>
2020-11-10 10:32:13 +0300

    fix alignment

--- Example/Example/ViewController.swift
+++ Example/Example/ViewController.swift
@@ -430,75 +430,74 @@ class ViewController: UIViewController {
     
     return configuration
   }
-
-private func updateTrimGalleryVideoConfiguration(_ configuration: TrimGalleryVideoConfiguration) -> TrimGalleryVideoConfiguration {
-var configuration = configuration
-
-configuration.backButtonConfiguration = BackButtonConfiguration(imageConfiguration: ImageConfiguration(imageName: "back_arrow"))
-configuration.playerControlConfiguration = PlayerControlConfiguration(
-playButtonImageName: "ic_play",
-pauseButtonImageName: "ic_pause"
-)
-
-configuration.galleryVideoPartsConfiguration.addGalleryVideoPartImageName = "add_video_part"
-configuration.deleteGalleryVideoPartButtonConfiguration = ImageButtonConfiguration(imageConfiguration: ImageConfiguration(imageName: "ic_delete_video_part"))
-
-configuration.nextButtonConfiguration.backgroundColor = .clear
-configuration.nextButtonConfiguration.textConfiguration.color = UIColor(red: 6, green: 188, blue: 193)
-
-configuration.editedTimeLabelConfiguration.errorColor = UIColor(red: 250, green: 62, blue: 118)
-
-return configuration
-}
-
-private func updateMultiTrimConfiguration(_ configuration: MultiTrimConfiguration) -> MultiTrimConfiguration {
-var configuration = configuration
-
-configuration.backButton = BackButtonConfiguration(imageConfiguration: ImageConfiguration(imageName: "back_arrow"))
-configuration.playerControlConfiguration = PlayerControlConfiguration(
-playButtonImageName: "ic_play",
-pauseButtonImageName: "ic_pause"
-)
-
-configuration.trimTimeLineConfiguration.draggerImageName = "trim_left"
-configuration.trimTimeLineConfiguration.doneButtonConfiguration.style.color = UIColor(red: 6, green: 188, blue: 193)
-configuration.trimTimeLineConfiguration.trimControlsColor = UIColor(red: 250, green: 62, blue: 118)
-
-configuration.saveButton.backgroundColor = .clear
-configuration.saveButton.textConfiguration.color = UIColor(red: 6, green: 188, blue: 193)
-
-configuration.editedTimeLabelConfiguration.errorColor = UIColor(red: 250, green: 62, blue: 118)
-
-return configuration
-}
-
-private func updateSingleTrimConfiguration(_ configuration: SingleTrimConfiguration) -> SingleTrimConfiguration {
-var configuration = configuration
-
-configuration.playerControlConfiguration = PlayerControlConfiguration(
-playButtonImageName: "ic_play",
-pauseButtonImageName: "ic_pause"
-)
-configuration.backButton = BackButtonConfiguration(imageConfiguration: ImageConfiguration(imageName: "back_arrow"))
-
-configuration.trimTimeLineConfiguration.draggerImageName = "trim_left"
-configuration.trimTimeLineConfiguration.trimControlsColor = UIColor(red: 250, green: 62, blue: 118)
-
-configuration.saveButton.backgroundColor = .clear
-configuration.saveButton.textConfiguration.color = UIColor(red: 6, green: 188, blue: 193)
-
-configuration.editedTimeLabelConfiguration.errorColor = UIColor(red: 250, green: 62, blue: 118)
-
-return configuration
-}
-
+  
+  private func updateTrimGalleryVideoConfiguration(_ configuration: TrimGalleryVideoConfiguration) -> TrimGalleryVideoConfiguration {
+    var configuration = configuration
+    
+    configuration.backButtonConfiguration = BackButtonConfiguration(imageConfiguration: ImageConfiguration(imageName: "back_arrow"))
+    configuration.playerControlConfiguration = PlayerControlConfiguration(
+      playButtonImageName: "ic_play",
+      pauseButtonImageName: "ic_trim_pause"
+    )
+    
+    configuration.galleryVideoPartsConfiguration.addGalleryVideoPartImageName = "add_video_part"
+    configuration.deleteGalleryVideoPartButtonConfiguration = ImageButtonConfiguration(imageConfiguration: ImageConfiguration(imageName: "ic_delete_video_part"))
+    
+    configuration.nextButtonConfiguration.backgroundColor = .clear
+    configuration.nextButtonConfiguration.textConfiguration.color = UIColor(red: 6, green: 188, blue: 193)
+    
+    configuration.editedTimeLabelConfiguration.errorColor = UIColor(red: 250, green: 62, blue: 118)
+    
+    return configuration
+  }
+  
+  private func updateMultiTrimConfiguration(_ configuration: MultiTrimConfiguration) -> MultiTrimConfiguration {
+    var configuration = configuration
+    
+    configuration.backButton = BackButtonConfiguration(imageConfiguration: ImageConfiguration(imageName: "back_arrow"))
+    configuration.playerControlConfiguration = PlayerControlConfiguration(
+      playButtonImageName: "ic_play",
+      pauseButtonImageName: "ic_trim_pause"
+    )
+    
+    configuration.trimTimeLineConfiguration.draggerImageName = "trim_left"
+    configuration.trimTimeLineConfiguration.doneButtonConfiguration.style.color = UIColor(red: 6, green: 188, blue: 193)
+    configuration.trimTimeLineConfiguration.trimControlsColor = UIColor(red: 250, green: 62, blue: 118)
+    
+    configuration.saveButton.backgroundColor = .clear
+    configuration.saveButton.textConfiguration.color = UIColor(red: 6, green: 188, blue: 193)
+    
+    configuration.editedTimeLabelConfiguration.errorColor = UIColor(red: 250, green: 62, blue: 118)
+    
+    return configuration
+  }
+  
+  private func updateSingleTrimConfiguration(_ configuration: SingleTrimConfiguration) -> SingleTrimConfiguration {
+    var configuration = configuration
+    
+    configuration.playerControlConfiguration = PlayerControlConfiguration(
+      playButtonImageName: "ic_play",
+      pauseButtonImageName: "ic_trim_pause"
+    )
+    configuration.backButton = BackButtonConfiguration(imageConfiguration: ImageConfiguration(imageName: "back_arrow"))
+    
+    configuration.trimTimeLineConfiguration.draggerImageName = "trim_left"
+    configuration.trimTimeLineConfiguration.trimControlsColor = UIColor(red: 250, green: 62, blue: 118)
+    
+    configuration.saveButton.backgroundColor = .clear
+    configuration.saveButton.textConfiguration.color = UIColor(red: 6, green: 188, blue: 193)
+    
+    configuration.editedTimeLabelConfiguration.errorColor = UIColor(red: 250, green: 62, blue: 118)
+    
+    return configuration
+  }
 }
 
 extension ViewController: BanubaVideoEditorSDKDelegate {
   func videoEditorDidCancel(_ videoEditor: BanubaVideoEditorSDK) {
     videoEditor.dismissVideoEditor(animated: true, completion: nil)
   }
-
+  
   func videoEditorDone(_ videoEditor: BanubaVideoEditorSDK) {
     videoEditor.dismissVideoEditor(animated: true, completion: nil)
   }
