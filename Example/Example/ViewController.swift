c6633d39a15bf5302ef9a32d5cdcebfdfe3a0784
ve-sdk-ios <73183216+ve-sdk-ios@users.noreply.github.com>
2021-02-15 11:40:51 +0300

    Update ViewController.swift

--- Example/Example/ViewController.swift
+++ Example/Example/ViewController.swift
@@ -72,6 +72,7 @@ class ViewController: UIViewController {
     config.updateFeatureConfiguration(featureConfiguration: featureConfiguration)
     
     config.isHandfreeEnabled = true
+    config.handsfreeConfiguration = updateHandsfreeConfiguration(config.handsfreeConfiguration)
     config.recorderConfiguration = updateRecorderConfiguration(config.recorderConfiguration)
     config.editorConfiguration = updateEditorConfiguration(config.editorConfiguration)
     config.galleryConfiguration = updateGalleryConfiguration(config.galleryConfiguration)
@@ -242,6 +243,27 @@ class ViewController: UIViewController {
     configuration.saveButton.title.style.color = .white
     
     return configuration
+  }
+  
+  private func updateHandsfreeConfiguration(_ configuration: HandsfreeConfiguration?) -> HandsfreeConfiguration? {
+    guard var config = configuration else { return nil }
+    config.timerOptionBarConfiguration.disableOptionTitle = "Off"
+    config.timerOptionBarConfiguration.selectorColor = UIColor.yellow
+    config.timerOptionBarConfiguration.selectorTextColor = UIColor.black
+    config.timerOptionBarConfiguration.optionBackgroundColor = UIColor.clear
+    config.timerOptionBarConfiguration.optionCornerRadius = 0.0
+    config.timerOptionBarConfiguration.optionTextColor = UIColor.white
+    config.timerOptionBarConfiguration.backgroundColor = UIColor.black
+    config.timerOptionBarConfiguration.sliderCornerRadius = 8.0
+    config.timerOptionBarConfiguration.barCornerRadius = 4.0
+    config.timerOptionBarConfiguration.selectorEdgeInsets = UIEdgeInsets(top: 0.0, left: 0.0, bottom: 0.0, right: 0.0)
+    config.timerOptionBarConfiguration.activeThumbAndLineColor = UIColor.yellow
+    config.timerOptionBarConfiguration.inactiveThumbAndLineColor = UIColor.white
+    config.timerOptionBarConfiguration.minVideoDuration = 1.0
+    config.timerOptionBarConfiguration.buttonCornerRadius = 20.0
+    config.timerOptionBarConfiguration.buttonBackgroundColor = UIColor.yellow
+    config.timerOptionBarConfiguration.switchOnTintColor = #colorLiteral(red: 1, green: 0.7529411765, blue: 0.01176470588, alpha: 0.3034032534)
+    return config
   }
   
   private func updateCombinedGalleryConfiguration(_ configuration: CombinedGalleryConfiguration) -> CombinedGalleryConfiguration {
