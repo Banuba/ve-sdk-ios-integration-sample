3c450fb55c5979ab4defc8b730edeebccf0d1fad
ve-sdk-ios <73183216+ve-sdk-ios@users.noreply.github.com>
2021-02-15 14:06:51 +0300

    Update ViewController.swift

--- Example/Example/ViewController.swift
+++ Example/Example/ViewController.swift
@@ -625,12 +625,17 @@ class ViewController: UIViewController {
   }
   
   private func updateAlertConfiguration(_ configuration: AlertViewConfiguration) -> AlertViewConfiguration {
-    var configuration = configuration
-    
-    configuration.refuseButtonBackgroundColor = UIColor(red: 6, green: 188, blue: 193)
-    configuration.refuseButtonTextColor = UIColor.white
-    
-    return configuration
+    var config = configuration
+    config.cornerRadius = 7.0
+    config.buttonRadius = 4.0
+    config.refuseButtonTextColor = UIColor.white
+    config.refuseButtonBackgroundColor = UIColor.green
+    config.agreeButtonBackgroundColor = UIColor.yellow
+    config.agreeButtonTextColor = UIColor.black
+    config.titleAligment = .center
+    config.titleFont = UIFont.boldSystemFont(ofSize: 22.0)
+    config.buttonsFont = UIFont.systemFont(ofSize: 16.0)
+    return config
   }
   
   private func updateFullScreenActivityConfiguration(_ configuration: FullScreenActivityConfiguration) -> FullScreenActivityConfiguration {
