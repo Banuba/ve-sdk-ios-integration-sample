2079e6dce0083256ee6c7f146c6453b879aaf969
ve-sdk-ios <73183216+ve-sdk-ios@users.noreply.github.com>
2021-02-15 17:13:25 +0300

    Update HandsfreeConfiguration.swift

--- Example/Example/Extension/HandsfreeConfiguration.swift
+++ Example/Example/Extension/HandsfreeConfiguration.swift
@@ -1,1 +1,22 @@
-
+extension ViewController {
+  private func updateHandsfreeConfiguration(_ configuration: HandsfreeConfiguration?) -> HandsfreeConfiguration? {
+    guard var config = configuration else { return nil }
+    config.timerOptionBarConfiguration.disableOptionTitle = "Off"
+    config.timerOptionBarConfiguration.selectorColor = UIColor.yellow
+    config.timerOptionBarConfiguration.selectorTextColor = UIColor.black
+    config.timerOptionBarConfiguration.optionBackgroundColor = UIColor.clear
+    config.timerOptionBarConfiguration.optionCornerRadius = 0.0
+    config.timerOptionBarConfiguration.optionTextColor = UIColor.white
+    config.timerOptionBarConfiguration.backgroundColor = UIColor.black
+    config.timerOptionBarConfiguration.sliderCornerRadius = 8.0
+    config.timerOptionBarConfiguration.barCornerRadius = 4.0
+    config.timerOptionBarConfiguration.selectorEdgeInsets = UIEdgeInsets(top: 0.0, left: 0.0, bottom: 0.0, right: 0.0)
+    config.timerOptionBarConfiguration.activeThumbAndLineColor = UIColor.yellow
+    config.timerOptionBarConfiguration.inactiveThumbAndLineColor = UIColor.white
+    config.timerOptionBarConfiguration.minVideoDuration = 1.0
+    config.timerOptionBarConfiguration.buttonCornerRadius = 20.0
+    config.timerOptionBarConfiguration.buttonBackgroundColor = UIColor.yellow
+    config.timerOptionBarConfiguration.switchOnTintColor = #colorLiteral(red: 1, green: 0.7529411765, blue: 0.01176470588, alpha: 0.3034032534)
+    return config
+  }
+}
