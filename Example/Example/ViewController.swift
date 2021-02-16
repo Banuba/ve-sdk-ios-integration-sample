e753a69460ef7babdc39211e5676a8e52f0d970e
ve-sdk-ios <73183216+ve-sdk-ios@users.noreply.github.com>
2021-02-16 21:56:43 +0300

    Update ViewController.swift

--- Example/Example/ViewController.swift
+++ Example/Example/ViewController.swift
@@ -110,36 +110,6 @@ class ViewController: UIViewController {
     return configuration
   }
   
-  private func updateVideCoverSelectionConfiguration(_ configuration: SimpleVideoCoverSelectionConfiguration) -> SimpleVideoCoverSelectionConfiguration {
-    var configuration = configuration
-    
-    configuration.cancelButton = TextButtonConfiguration(
-      style: TextConfiguration(
-        font: UIFont.boldSystemFont(ofSize: 18.0),
-        color: UIColor(red: 6, green: 188, blue: 193)
-      ),
-      text: "Cancel"
-    )
-    configuration.doneButton = RoundedButtonConfiguration(
-      textConfiguration: TextConfiguration(
-        font: UIFont.boldSystemFont(ofSize: 18.0),
-        color: UIColor(red: 6, green: 188, blue: 193)
-      ),
-      cornerRadius: 0.0,
-      backgroundColor: .clear
-    )
-    configuration.sliderColor = UIColor(red: 6, green: 188, blue: 193)
-    configuration.sliderMinTrackTintColor = UIColor(red: 6, green: 188, blue: 193)
-    configuration.toolTipLabel = TextConfiguration(
-      kern: 0.0,
-      font: UIFont.systemFont(ofSize: 16.0),
-      color: .white,
-      alignment: .left
-    )
-    
-    return configuration
-  }
-  
   private func updateSpeedSelectionConfiguration(_ configuration: SpeedSelectionConfiguration) -> SpeedSelectionConfiguration {
     var configuration = configuration
     
