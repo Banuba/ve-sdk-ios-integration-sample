07b151e1abd61c322649cf0f63cfcf117951ab56
ve-sdk-ios <sdk-ve-ios@banuba.com>
2021-05-05 16:27:12 +0300

    migrate to version 1.0.15.8

---
+++ Example/Example/Helpers/AudioBrowserConfigurator.swift
@@ -0,0 +1,17 @@
+//
+//  AudioBrowserConfigurator.swift
+//  VideoEditorSDKSandbox
+//
+//  Created by Andrei Sak on 17.11.20.
+//  Copyright © 2020 Banuba. All rights reserved.
+//
+
+import BanubaAudioBrowserSDK
+import BanubaMusicEditorSDK
+import UIKit
+
+struct AudioBrowserConfigurator {
+  static func configure() {
+    AudioBrowserConfig.shared.mubertAudioConfig.pat = "Put mubert pat token"
+  }
+}
