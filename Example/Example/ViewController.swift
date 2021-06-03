570879d311ef1b837a0d46161e8285cefe232074
Ruslan Filistovich <ruslan.filistovich@banuba.com>
2021-06-03 16:18:48 +0300

    Add cover configuration file

--- Example/Example/ViewController.swift
+++ Example/Example/ViewController.swift
@@ -79,6 +79,7 @@ class ViewController: UIViewController {
     config.editorConfiguration = updateEditorConfiguration(config.editorConfiguration)
     config.combinedGalleryConfiguration = updateCombinedGalleryConfiguration(config.combinedGalleryConfiguration)
     config.videoCoverSelectionConfiguration = updateVideCoverSelectionConfiguration(config.videoCoverSelectionConfiguration)
+    config.extendedVideoCoverSelectionConfiguration = updateVideCoverSelectionConfiguration(config.extendedVideoCoverSelectionConfiguration)
     config.musicEditorConfiguration = updateMusicEditorConfigurtion(config.musicEditorConfiguration)
     config.overlayEditorConfiguration = updateOverlayEditorConfiguraiton(config.overlayEditorConfiguration)
     config.textEditorConfiguration = updateTextEditorConfiguration(config.textEditorConfiguration)
@@ -92,36 +93,6 @@ class ViewController: UIViewController {
     config.handsfreeConfiguration = updateHandsfreeConfiguration(config.handsfreeConfiguration)
     
     return config
-  }
-  
-  private func updateVideCoverSelectionConfiguration(_ configuration: SimpleVideoCoverSelectionConfiguration) -> SimpleVideoCoverSelectionConfiguration {
-    var configuration = configuration
-    
-    configuration.cancelButton = TextButtonConfiguration(
-      style: TextConfiguration(
-        font: UIFont.boldSystemFont(ofSize: 18.0),
-        color: UIColor(red: 6, green: 188, blue: 193)
-      ),
-      text: "Cancel"
-    )
-    configuration.doneButton = RoundedButtonConfiguration(
-      textConfiguration: TextConfiguration(
-        font: UIFont.boldSystemFont(ofSize: 18.0),
-        color: UIColor(red: 6, green: 188, blue: 193)
-      ),
-      cornerRadius: 0.0,
-      backgroundColor: .clear
-    )
-    configuration.sliderColor = UIColor(red: 6, green: 188, blue: 193)
-    configuration.sliderMinTrackTintColor = UIColor(red: 6, green: 188, blue: 193)
-    configuration.toolTipLabel = TextConfiguration(
-      kern: 0.0,
-      font: UIFont.systemFont(ofSize: 16.0),
-      color: .white,
-      alignment: .left
-    )
-    
-    return configuration
   }
   
   private func updateSpeedSelectionConfiguration(_ configuration: SpeedSelectionConfiguration) -> SpeedSelectionConfiguration {
