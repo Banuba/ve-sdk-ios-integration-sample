58555776d691a7194a9e75afe104ec822aeac257
Gleb Markin <gleb.markin@banuba.com>
2021-09-16 14:33:47 +0300

    Add SPM dependency instead of Cocoapods

--- Example/Example/Extension/AlertViewConfiguration.swift
+++ Example/Example/Extension/AlertViewConfiguration.swift
@@ -5,14 +5,30 @@ extension ViewController {
   func updateAlertViewConfiguration(_ configuration: AlertViewConfiguration) -> AlertViewConfiguration {
     var config = configuration
     config.cornerRadius = 7.0
-    config.buttonRadius = 4.0
-    config.refuseButtonTextColor = UIColor.white
+    config.refuseButtonRadius = 4.0
+    config.agreeButtonRadius = 4.0
+
     config.refuseButtonBackgroundColor = #colorLiteral(red: 0.2350233793, green: 0.7372031212, blue: 0.7565478683, alpha: 1)
     config.agreeButtonBackgroundColor = #colorLiteral(red: 0.07058823529, green: 0.1490196078, blue: 0.2274509804, alpha: 1)
-    config.agreeButtonTextColor = UIColor.white
-    config.titleAligment = .center
-    config.titleFont = UIFont.boldSystemFont(ofSize: 22.0)
-    config.buttonsFont = UIFont.systemFont(ofSize: 16.0)
+    
+    config.refuseButtonTextConfig = TextButtonConfiguration(
+      style: TextConfiguration(
+        font: UIFont.systemFont(ofSize: 16.0),
+        color: .white
+      )
+    )
+    config.agreeButtonTextConfig = TextButtonConfiguration(
+      style: TextConfiguration(
+        font: UIFont.systemFont(ofSize: 16.0),
+        color: .white
+      )
+    )
+    
+    config.titleTextConfig = TextConfiguration(
+      font: UIFont.boldSystemFont(ofSize: 22.0),
+      color: .blue
+    )
+    
     return config
   }
 }
--- Example/Example/ViewController.swift
+++ Example/Example/ViewController.swift
@@ -32,7 +32,7 @@ class ViewController: UIViewController {
     viewControllerFactory.exposureViewFactory = DefaultExposureViewFactory()
     
     videoEditorSDK = BanubaVideoEditor(
-      token: "Put video editor token here",
+      token: "5a9vX8ua6fgsFHchSkZ6GTRKfEC8pkAjGbiOTzWJixLY5BOP5V4/JGzPALOAT7pG4MwQLkzuMrhMDHv/ZDS+ZPxpl5zcY+rUT8A2f6fIwjwdy5QoeXyynmi6lQrI1/OKDUjsM5puo5gflQSOUHVVEl/glE9EW/YUgtnywcKV91x1eGE1Vnm0MRrTY1KOWbYlD7Bvb7gh69Hce9m8033ObVZyfVD8FbLpWRnUzX7Sqhm4GKlo3NisOJEKtoxZp2Pp2Hs/r+Id24feocpdyOPCmYxBxCxosAMNsd7dj+d8uD8iVVXbyeMnKYNO8DWuxasyCBb9QLTn9HcU4JTpYzOcBfNflYHXyUdec19XmnTM8DAiMXoT1G2xmlZYgrUHIGhiWI0/owGiq2Prmv+6MU9XB7wj5IK3DMGuCwZ4rqlWfe41nFJv6UbfnpPDcF5zINiMfwuC+7lbHdHtH5g2jnpIxdywkEln4dy78mgPdbRPrQvwiWIsGiXwdjkikZxf6ARWFHgHV7Bn63S8B63oxS2NSduCIllHdhowcdJZCzWjCkP3AcTmuAz0rD3BynPCrwgEOL58g3GT+w7ZbNX8JDVE9mZVKORCRp7iMmNsR6Uxy/egcm5ylaV1QfBS6xaxK0gZLimBsvc6V2uVovMM0Ww+4pi9mL5y12toVOBZyMUVr9QcMFE2aVw3VAtJWlLXaiqUddXxWOTyxsJ5No0/Ht5ME5xADJWKK+Vi/U1SUMRPBnkcw+nRcfTsWgJ1yh449VVpW7BnUbkBpFDUeEaF3Y23dT0jr9ZTpjeRqqS9Fro32bFL7KfL02e7vd2UKCuUk/UXJsa2PNcGJaGBMCajP4M1pwiiyhUWuEcFBG90AYBBlEtURAEMdJM1ym6WytEr9G1XBka9e1Fwp+tWDarDoQx1X0t50bFFh0v/CALsEjh+6UYTPPI/E79cBS4IrezwfE6zmEVurNO81pFz67LHnhI7cC0o9lkIH8iS86zWqng9qNdJ0PnFwrEbLAjImdmq+LQcUxYwByancQ4lRQOrZvZHLfkowtkZ8ZYjmBEQ4+cd/IJf4K+MgQYwXNt+YFosS7q4KY5F2uzIlA7g0/KHR1x9Bp8ntGFeiiaDhnYXXkdoz5lFVHTNvXECpQxIrmVBOh+CIfd4cEt5EuPW29KOiWqgRAM7DNfimQZ3tBit68Vhl1Bhv1dR32wPtPz+83UobsuH+IPrd6FbgnMFv7mPlvDEnu15VPG73mKh7OCTY4QZ87j0OjBXNBBVZlRDbcb9ynopC6Ftlrkt1uchVa4edFaaSd8E37jjOZbrDFJcBcbdFBAKtoc8BqeYuVr1WAzQhSvGoiemZImPkL2TT7xlyn/7QPhNNdInc/f5ObvZCc7FLkP+1xXAL7PcZaXLZcu64K89i/A+zClE4qDYl0zd5+cYTeQeN4GuTs0LQ3ETFWaODa2hkTLNmZS9Q6JpSbfAnQ4lOpUWcojKKLrjQUfyrrLsAKjKa1/6qnSmrOJsWThajOfCTlZ4zTn9B0Dj5IVtu1lJRbPlWpW65rbfasISnMi6sEgU6JrW7/sFrpa4M2eexR1t1xMfzmqBwhxtFMvfBKiy+7MJgJYPwCcUDlEBN3/VzTFmru4JpspsA1uro/tUOv9JvaXAyLSzn9cj2Muy2AHuk+OM/U53AEoz1L8TW5dd+g+xRlLOIXPvzxTOmGLQrCop2NYMMYtwXOp4CUrekvADNtvJAY+UnVV6WeiDKfE3raPuyGMwJBKOlPHH2A7yfgQK4Q364WX/ZwlmmLf8ntfGifzlkeQcfeVEILUy8bUMbwUYrlTcDqfZLAdrFmHrMvmTATd3a3y5C33iMsZIi6tsSQ0QN/qRdoEsxinD6fBZwAZzNL9bhN+uQkYj85UHnPk0yhPoq4U5cwgci5/iF0yq0w0txTrv/CUYZshPGqH8O1hXo4e9AhT639ld2IaomnddPWByehn7GncmJw4y8OCebihrZ0iVbNadl3CK9ljU3nbNHNhnwxyKRyxey9MVmNl2u/Yoh9iAnpgEdhD3IiGPlPxc3AjGDYGTe305ww0x3r39GeEWcujUdEx3N8uQlnemzuL8YvUmWT7H3XelSEc0TLGPUhpcK0R8VBG3wi9VYqGRIO0ZDWYFhyV1FEwqn2y4whVazTgMrYMbRb6MWG9+xpqjlygnTELK4eY7KeAGTTfQR2nXort7w/g0wJFgDaSCUsV/UVyQhC1ZDeo6bMEsnIh2nyoVEl6f2LQ1FWx6pO1YsH26iK5ozSh0bFBdC2F/Mp4pB8FGP5+6wvAWUWcA0JXIYe+SLeO+ajNsTu/XZg==",
       configuration: config,
       analytics: Analytics(),
       externalViewControllerFactory: viewControllerFactory
@@ -165,10 +165,16 @@ extension ViewController {
       fileURL: videoURL,
       quality: .auto,
       useHEVCCodecIfPossible: true,
-      watermarkConfiguration: watermarkConfiguration,
+      watermarkConfiguration: watermarkConfiguration
+    )
+    
+    let config = ExportConfiguration(
+      videoConfigurations: [exportConfiguration],
+      isCoverEnabled: true,
       gifSettings: GifSettings(duration: 0.3)
     )
-    videoEditorSDK?.exportVideos(using: [exportConfiguration], completion: { (success, error) in
+    
+    videoEditorSDK?.export(using: config, completion: { (success, error, exportCoverImages)  in
       DispatchQueue.main.async {
         // Clear video editor session data
         self.videoEditorSDK?.clearSessionData()
@@ -224,9 +230,7 @@ extension ViewController {
         PHImageManager.default().requestAVAsset(
           forVideo: asset,
           options: .none
-        ) { [weak self] (asset, _, _) in
-          
-          guard let self = self else { return }
+        ) { (asset, _, _) in
           guard let asset = asset else { return }
           
           let groupHandler = {
