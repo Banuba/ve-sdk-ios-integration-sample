1e5a6f48e783d5b7747949eb721579f24ea6a9ea
Gleb Markin <gleb.markin@banuba.com>
2021-11-26 20:35:02 +0300

    Gallery config

--- Example/Example/Extension/CombinedGalleryConfiguration.swift
+++ Example/Example/Extension/CombinedGalleryConfiguration.swift
@@ -5,7 +5,9 @@ import VideoEditor
 extension ViewController {
   func updateCombinedGalleryConfiguration(_ configuration: GalleryConfiguration) -> GalleryConfiguration {
     configuration.videoResolution = .hd1920x1080
-    configuration.galleryItemConfiguration.orderNumberBackgroudColor = UIColor.yellow
+    configuration.draftsButtonConfiguration.textConfiguration.color = #colorLiteral(red: 0.2350233793, green: 0.7372031212, blue: 0.7565478683, alpha: 1)
+    configuration.nextButtonConfiguration.backgroundColor = #colorLiteral(red: 0.2350233793, green: 0.7372031212, blue: 0.7565478683, alpha: 1)
+    configuration.galleryItemConfiguration.orderNumberBackgroudColor = #colorLiteral(red: 0.2350233793, green: 0.7372031212, blue: 0.7565478683, alpha: 1)
     configuration.galleryItemConfiguration.orderNumberTitleColor = UIColor.black
     configuration.galleryItemConfiguration.backgroundSelectionIndicatorImageName = "elipce"
     configuration.galleryItemConfiguration.hideSelectionIndicatorBySelection = true
@@ -14,14 +16,14 @@ extension ViewController {
     configuration.galleryItemConfiguration.activityIndicatorConfiguration.activityLineWidth = 2.0
     configuration.galleryItemConfiguration.cornerRadius = 0.0
     configuration.closeButtonConfiguration.imageConfiguration = BanubaVideoEditorSDK.ImageConfiguration(imageName: "camera_control.back")
-    configuration.albumButtonConfiguration.style.color = UIColor.yellow
+    configuration.albumButtonConfiguration.style.color = #colorLiteral(red: 0.2350233793, green: 0.7372031212, blue: 0.7565478683, alpha: 1)
     configuration.nextButtonConfiguration.text = "Next"
     configuration.noItemsLabelConfiguration.alignment = .center
     configuration.layoutConfiguration.numberOfItemsPerRow = 2
     configuration.topBarBlurColor = UIColor.yellow
     configuration.clearSelectionButtonConfiguration.imageConfiguration = BanubaVideoEditorSDK.ImageConfiguration(imageName: "camera_control.back")
-    configuration.galleryTypeButton.style.color = UIColor.yellow
-    configuration.galleryTypeUnderlineColor = UIColor.yellow
+    configuration.galleryTypeButton.style.color = #colorLiteral(red: 0.2350233793, green: 0.7372031212, blue: 0.7565478683, alpha: 1)
+    configuration.galleryTypeUnderlineColor = #colorLiteral(red: 0.2350233793, green: 0.7372031212, blue: 0.7565478683, alpha: 1)
     configuration.visibleTabsInGallery = [.video, .photo]
     
     return configuration
