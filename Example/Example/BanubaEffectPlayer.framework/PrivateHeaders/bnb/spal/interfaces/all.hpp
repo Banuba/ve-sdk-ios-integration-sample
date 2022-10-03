dfab6775a59ff6846552172d64bbc47f028cf1ca
sdk-banuba <71497187+sdk-banuba@users.noreply.github.com>
2022-10-03 12:17:15 +0300

    remove unused EP

--- Example/Example/BanubaEffectPlayer.framework/PrivateHeaders/bnb/spal/interfaces/all.hpp
+++
@@ -1,4 +0,0 @@
-#pragma once
-
-#include <bnb/spal/interfaces/frames_provider.hpp>
-#include <bnb/spal/interfaces/types.hpp>
--- Example/Example/BanubaEffectPlayer.framework/PrivateHeaders/bnb/utils/interfaces/all.hpp
+++
@@ -1,7 +0,0 @@
-#pragma once
-
-#include <bnb/utils/interfaces/hardware_class.hpp>
-#include <bnb/utils/interfaces/log_record_callback.hpp>
-#include <bnb/utils/interfaces/severity_level.hpp>
-#include <bnb/utils/interfaces/utility_manager.hpp>
-#include <bnb/utils/interfaces/types.hpp>
