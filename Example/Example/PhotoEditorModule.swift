1319db3ce0598e8ecb42687ee084b1696310aec5
sdk-ve-ios <sdk-ve-ios@banuba.com>
2026-03-11 17:13:17 +0300

    Redesign the VideoEditorModule and PhotoEditorModule by encapsulating exports and simplifying integration

--- Example/Example/PhotoEditorModule.swift
+++ Example/Example/PhotoEditorModule.swift
@@ -1,20 +1,47 @@
 import BanubaPhotoEditorSDK
 
-class PhotoEditorModule {
-    var photoEditorSDK: BanubaPhotoEditor?
-    
-    init(token: String) {
+final class PhotoEditorModule: BanubaPhotoEditorDelegate {
+
+    /// Callback for handling editing result
+    let onPhotoEditingFinished: (UIImage) -> Void
+
+    let photoEditorSDK: BanubaPhotoEditor
+
+    init?(token: String, onPhotoEditingFinished: @escaping (UIImage) -> Void) {
+        self.onPhotoEditingFinished = onPhotoEditingFinished
+
         let configuration = PhotoEditorConfig()
-        photoEditorSDK = BanubaPhotoEditor(
+
+        guard let photoEditorSDK = BanubaPhotoEditor(
             token: token,
             configuration: configuration
-        )
+        ) else {
+            return nil
+        }
+
+        self.photoEditorSDK = photoEditorSDK
+        photoEditorSDK.delegate = self
     }
-    
+
+    func getLicenseState(completion: @escaping (_ isValid: Bool) -> Void) {
+        photoEditorSDK.getLicenseState(completion: completion)
+    }
+
     func presentPhotoEditor(with launchConfig: PhotoEditorLaunchConfig) {
-        photoEditorSDK?.presentPhotoEditor(
+        photoEditorSDK.presentPhotoEditor(
             withLaunchConfiguration: launchConfig,
             completion: nil
         )
+    }
+
+    // MARK: - BanubaPhotoEditorDelegate
+    func photoEditorDidCancel(_ photoEditor: BanubaPhotoEditorSDK.BanubaPhotoEditor) {
+        photoEditor.dismissPhotoEditor(animated: true, completion: nil)
+    }
+
+    func photoEditorDidFinishWithImage(_ photoEditor: BanubaPhotoEditorSDK.BanubaPhotoEditor, image: UIImage) {
+        photoEditor.dismissPhotoEditor(animated: true) {
+            self.onPhotoEditingFinished(image)
+        }
     }
 }
