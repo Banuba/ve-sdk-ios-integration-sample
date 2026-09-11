d4df22388c1668dc46b0839327ab683c99472af1
sdk-ve-ios <sdk-ve-ios@banuba.com>
2026-09-11 12:37:33 +0300

    Migrate VE to 1.54.2 and PE to 1.4.3

--- Example/Example/PhotoEditorModule.swift
+++ Example/Example/PhotoEditorModule.swift
@@ -2,44 +2,44 @@ import BanubaPhotoEditorSDK
 
 @MainActor
 final class PhotoEditorModule: BanubaPhotoEditorDelegate {
-
+  
   /// Callback for handling editing result
   let onPhotoEditingFinished: (UIImage) -> Void
-
+  
   let photoEditorSDK: BanubaPhotoEditor
-
+  
   init?(token: String, onPhotoEditingFinished: @escaping (UIImage) -> Void) {
     self.onPhotoEditingFinished = onPhotoEditingFinished
-
+    
     let configuration = PhotoEditorConfig()
-
+    
     guard let photoEditorSDK = BanubaPhotoEditor(
       token: token,
       configuration: configuration
     ) else {
       return nil
     }
-
+    
     self.photoEditorSDK = photoEditorSDK
     photoEditorSDK.delegate = self
   }
-
+  
   func getLicenseState(completion: @escaping (_ isValid: Bool) -> Void) {
     photoEditorSDK.getLicenseState(completion: completion)
   }
-
+  
   func presentPhotoEditor(with launchConfig: PhotoEditorLaunchConfig) {
     photoEditorSDK.presentPhotoEditor(
       withLaunchConfiguration: launchConfig,
       completion: nil
     )
   }
-
+  
   // MARK: - BanubaPhotoEditorDelegate
   func photoEditorDidCancel(_ photoEditor: BanubaPhotoEditor) {
     photoEditor.dismissPhotoEditor(animated: true, completion: nil)
   }
-
+  
   func photoEditorDidFinishWithImage(_ photoEditor: BanubaPhotoEditor, image: UIImage) {
     photoEditor.dismissPhotoEditor(animated: true) {
       self.onPhotoEditingFinished(image)
